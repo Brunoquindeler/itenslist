b68f348b19928676c9df283df9cef18f6fcf4919
Bruno Quindeler <bruno.quindeler@gmail.com>
2020-05-21 06:57:48 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### Projeto do curso de JavaScript da RocketSeat
---
+++ static/js/itens.js
@@ -0,0 +1,54 @@
+var listElement = document.querySelector('#list');
+var inputElement = document.querySelector('#inputAdd');
+var buttonElement = document.querySelector('#add');
+
+var frutas = JSON.parse(localStorage.getItem('list_frutas')) || [];
+
+function renderFrutas() {
+    listElement.innerHTML = '';
+
+    for (fruta of frutas) {
+        var frutasElement = document.createElement('li')
+        frutasElement.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-center')
+        frutasElement.style = "width: 100%; margin-bottom: 1%;"
+        var frutasText = document.createTextNode(fruta)
+        frutasElement.appendChild(frutasText);
+        
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+        linkElement.setAttribute('class', 'badge badge-danger badge-pill');
+        var linkText = document.createTextNode('Excluir');
+        linkElement.appendChild(linkText);       
+        
+        var pos = frutas.indexOf(fruta);
+        linkElement.setAttribute('onclick', 'deleteFruta(' + pos +')');
+
+        listElement.appendChild(frutasElement);
+        frutasElement.appendChild(linkElement);
+    }
+};
+
+renderFrutas();
+
+function addFrutas() {
+    var frutasText = inputElement.value;
+
+    if (frutasText != '') {
+    frutas.push(frutasText);
+    inputElement.value = '';
+    renderFrutas();
+    saveToStorage();
+    }
+};
+
+buttonElement.onclick = addFrutas;
+
+function deleteFruta(pos) {
+    frutas.splice(pos, 1);
+    renderFrutas();
+    saveToStorage();
+};
+
+function saveToStorage() {
+    localStorage.setItem('list_frutas', JSON.stringify(frutas))
+}
