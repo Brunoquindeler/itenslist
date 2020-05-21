652c0899ca1b85ba28264966d0d1774b64a63228
Bruno Quindeler <bruno.quindeler@gmail.com>
2020-05-21 16:06:25 -0300

    Add Prepare String

--- static/js/itens.js
+++ static/js/itens.js
@@ -2,17 +2,17 @@ var listElement = document.querySelector('#list');
 var inputElement = document.querySelector('#inputAdd');
 var buttonElement = document.querySelector('#add');
 
-var frutas = JSON.parse(localStorage.getItem('list_frutas')) || [];
+var itens = JSON.parse(localStorage.getItem('list_itens')) || [];
 
-function renderFrutas() {
+function renderItens() {
     listElement.innerHTML = '';
 
-    for (fruta of frutas) {
-        var frutasElement = document.createElement('li')
-        frutasElement.setAttribute('class', 'list-group-item list-group-item-primary d-flex justify-content-between align-items-center')
-        frutasElement.style = "width: 100%; margin-bottom: 1%; margin-top: 1%;"
-        var frutasText = document.createTextNode(fruta)
-        frutasElement.appendChild(frutasText);
+    for (item of itens) {
+        var itensElement = document.createElement('li')
+        itensElement.setAttribute('class', 'list-group-item list-group-item-primary d-flex justify-content-between align-items-center')
+        itensElement.style = "width: 100%; margin-bottom: 1%; margin-top: 1%;"
+        var itensText = document.createTextNode(item)
+        itensElement.appendChild(itensText);
         
         var linkElement = document.createElement('a');
         linkElement.setAttribute('href', '#');
@@ -20,66 +20,79 @@ function renderFrutas() {
         var linkText = document.createTextNode('Excluir');
         linkElement.appendChild(linkText);       
         
-        var pos = frutas.indexOf(fruta);
-        linkElement.setAttribute('onclick', 'deleteFruta(' + pos +')');
+        var pos = itens.indexOf(item);
+        linkElement.setAttribute('onclick', 'deleteItem(' + pos +')');
 
-        listElement.appendChild(frutasElement);
-        frutasElement.appendChild(linkElement);
+        listElement.appendChild(itensElement);
+        itensElement.appendChild(linkElement);
     }
 };
 
-renderFrutas();
+renderItens();
 
-function addFrutas() {
-    var frutasText = inputElement.value;
+// Transforma texto em minusculo e primeira letra em maiúsculo
+function transformText(text) {
+    var words = text.toLowerCase().split(" ");
+    for (var a = 0; a < words.length; a++) {
+        var w = words[a];
+        words[a] = w[0].toUpperCase() + w.slice(1);
+    }
+    return words.join(" ");
+};
 
-    var conteudo = frutas.indexOf(frutasText) != -1 ? 'True' : 'False';
+
+function addItens() {
+    // var itensText = inputElement.value.toLowerCase();
+    var itensText = transformText(inputElement.value)
+
+    var conteudo = itens.indexOf(itensText) != -1 ? 'True' : 'False';
 
 
-    if (frutasText == ''){
+    if (itensText == ''){
         alert("Campo vazio...");
         inputElement.value = '';
-    } else if (frutasText.length > 20) {
+    } else if (itensText.length > 20) {
         alert("Palavra muito grande...");
         inputElement.value = '';
     } else if (conteudo == 'True') {
         alert("Item já existe...");
         inputElement.value = '';
     } else {
-        frutas.push(frutasText);
+        itens.push(itensText);
         inputElement.value = '';
-        renderFrutas();
+        renderItens();
         saveToStorage();
-        console.log(frutasText.length)
+        console.log(itensText.length)
+        console.log(itensText)
     }
 
-    // if (frutasText == '' || frutasText.length > 20 || conteudo == 'True') {
+    // if (itensText == '' || itensText.length > 20 || conteudo == 'True') {
     //     alert("Campo vazio, já existe ou muito grande...")
     //     inputElement.value = '';
     // } else {
-    //     frutas.push(frutasText);
+    //     itens.push(itensText);
     //     inputElement.value = '';
-    //     renderFrutas();
+    //     renderItens();
     //     saveToStorage();
-    //     console.log(frutasText.length)
+    //     console.log(itensText.length)
     // }
 };
 
-buttonElement.onclick = addFrutas;
+buttonElement.onclick = addItens;
 
-function deleteFruta(pos) {
-    frutas.splice(pos, 1);
-    renderFrutas();
+function deleteItem(pos) {
+    itens.splice(pos, 1);
+    renderItens();
     saveToStorage();
 };
 
 function saveToStorage() {
-    localStorage.setItem('list_frutas', JSON.stringify(frutas))
+    localStorage.setItem('list_itens', JSON.stringify(itens))
 }
 
 inputElement.addEventListener('keyup', function(e){
   var key = e.which || e.keyCode;
   if (key == 13) { // codigo da tecla enter
-    addFrutas()
+    addItens()
   }
 });
