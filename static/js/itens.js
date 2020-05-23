56ff458fa4e2c6c0d5e68464c577d8be256adbdc
Bruno Quindeler <bruno.quindeler@gmail.com>
2020-05-23 02:26:15 -0300

    ...

--- static/js/itens.js
+++ static/js/itens.js
@@ -2,17 +2,17 @@ var listElement = document.querySelector('#list');
 var inputElement = document.querySelector('#inputAdd');
 var buttonElement = document.querySelector('#add');
 
-var itens = JSON.parse(localStorage.getItem('list_itens')) || [];
+var items = JSON.parse(localStorage.getItem('list_items')) || [];
 
-function renderItens() {
+function renderItems() {
     listElement.innerHTML = '';
 
-    for (item of itens) {
-        var itensElement = document.createElement('li')
-        itensElement.setAttribute('class', 'list-group-item list-group-item-primary d-flex justify-content-between align-items-center')
-        itensElement.style = "width: 100%; margin-bottom: 1%; margin-top: 1%;"
-        var itensText = document.createTextNode(item)
-        itensElement.appendChild(itensText);
+    for (item of items) {
+        var itemsElement = document.createElement('li')
+        itemsElement.setAttribute('class', 'list-group-item list-group-item-primary d-flex justify-content-between align-items-center')
+        itemsElement.style = "width: 100%; margin-bottom: 1%; margin-top: 1%;"
+        var itemsText = document.createTextNode(item)
+        itemsElement.appendChild(itemsText);
         
         var linkElement = document.createElement('a');
         linkElement.setAttribute('href', '#');
@@ -20,15 +20,15 @@ function renderItens() {
         var linkText = document.createTextNode('Excluir');
         linkElement.appendChild(linkText);       
         
-        var pos = itens.indexOf(item);
+        var pos = items.indexOf(item);
         linkElement.setAttribute('onclick', 'deleteItem(' + pos +')');
 
-        listElement.appendChild(itensElement);
-        itensElement.appendChild(linkElement);
+        listElement.appendChild(itemsElement);
+        itemsElement.appendChild(linkElement);
     }
 };
 
-renderItens();
+renderItems();
 
 // Transforma texto em minusculo e primeira letra em maiúsculo
 function transformText(text) {
@@ -41,58 +41,58 @@ function transformText(text) {
 };
 
 
-function addItens() {
-    // var itensText = inputElement.value.toLowerCase();
-    var itensText = transformText(inputElement.value)
+function addItems() {
+    // var itemsText = inputElement.value.toLowerCase();
+    var itemsText = transformText(inputElement.value)
 
-    var conteudo = itens.indexOf(itensText) != -1 ? 'True' : 'False';
+    var conteudo = items.indexOf(itemsText) != -1 ? 'True' : 'False';
 
-    if (itensText == ''){
+    if (itemsText == ''){
         alert("Campo vazio...");
         inputElement.value = '';
-    } else if (itensText.length > 20) {
+    } else if (itemsText.length > 20) {
         alert("Palavra muito grande...");
         inputElement.value = '';
     } else if (conteudo == 'True') {
         alert("Item já existe...");
         inputElement.value = '';
     } else {
-        itens.push(itensText);
+        items.push(itemsText);
         inputElement.value = '';
-        itens.sort();
-        renderItens();
+        items.sort();
+        renderItems();
         saveToStorage();
-        console.log(itensText.length)
-        console.log(itensText)
+        console.log(itemsText.length)
+        console.log(itemsText)
     }
 
-    // if (itensText == '' || itensText.length > 20 || conteudo == 'True') {
+    // if (itemsText == '' || itemsText.length > 20 || conteudo == 'True') {
     //     alert("Campo vazio, já existe ou muito grande...")
     //     inputElement.value = '';
     // } else {
-    //     itens.push(itensText);
+    //     items.push(itemsText);
     //     inputElement.value = '';
-    //     renderItens();
+    //     renderItems();
     //     saveToStorage();
-    //     console.log(itensText.length)
+    //     console.log(itemsText.length)
     // }
 };
 
-buttonElement.onclick = addItens;
+buttonElement.onclick = addItems;
 
 function deleteItem(pos) {
-    itens.splice(pos, 1);
-    renderItens();
+    items.splice(pos, 1);
+    renderItems();
     saveToStorage();
 };
 
 function saveToStorage() {
-    localStorage.setItem('list_itens', JSON.stringify(itens))
+    localStorage.setItem('list_items', JSON.stringify(items))
 }
 
 inputElement.addEventListener('keyup', function(e){
   var key = e.which || e.keyCode;
   if (key == 13) { // codigo da tecla enter
-    addItens()
+    addItems()
   }
 });
